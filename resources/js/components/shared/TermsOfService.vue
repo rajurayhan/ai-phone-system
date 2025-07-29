8223b1a5f890e09137e253f28df0ed54a0baacc0
Raju Rayhan <raju.rayhan@yandex.com>
2025-07-30 03:57:20 +0600

    Added sulus number

--- resources/js/components/shared/TermsOfService.vue
+++ resources/js/components/shared/TermsOfService.vue
@@ -100,7 +100,7 @@
           </p>
           <div class="bg-gray-50 p-4 rounded-lg">
             <p class="mb-2"><strong>Email:</strong> support@SulusAI.com</p>
-            <p><strong>Phone:</strong> (231) 444-5797
+            <p><strong>Phone:</strong> (682) 582 8396
             </p>
           </div>
         </div>
